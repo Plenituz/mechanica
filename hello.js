89903e2ff82e5b2cf257c3721921698beda2cda2
Plenituz <Plenituz@gmail.com>
2017-06-07 18:44:05 +0200

    base node express

---
+++ hello.js
@@ -0,0 +1,29 @@
+//equivalent of include in C :
+var http = require('http');
+var express = require('express');
+var path = require('path');
+var EJS = require('ejs');
+
+var app = express();
+//tell express to use EJS render even for html files
+app.engine('html', EJS.renderFile);
+//set the view engine to ejs
+app.set('view engine', 'ejs');
+//indicate to express where the views directory is
+app.set('views', path.join(__dirname, 'views'));
+
+console.log("prep");
+
+app.get('/', function(req, res) {
+	console.log("connect /");
+	//render "accueil.html" using the set render engine (ejs)
+	res.render('accueil.html')
+})
+.use(function(req, res, next){
+	//in case the user asked for an unset page 
+	console.log("connect 404");
+	res.writeHead(200, {"Content-Type": "text/html"});
+    res.end('<p>404 not found, bitch</p>');
+});
+//listen on port 80 (http)
+app.listen(80);
