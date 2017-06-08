1f1b5d8a7316710ad8b6610a0cfbd138d0c643bf
Plenituz <Plenituz@gmail.com>
2017-06-08 20:05:49 +0200

    exemple dustjs
    tape ip/exemple pour voir le resultat

--- hello.js
+++ hello.js
@@ -8,10 +8,8 @@ var defaultImgPath = path.join(__dirname, 'public', 'imgs', 'default.png');
 var doCache = false;//TODO quand on passe en prod faut changer ca
 
 var app = express();
-//tell express to use EJS render even for html files
-//app.engine('html', hoffman.renderFile);
 app.engine('dust', hoffman.__express());
-//set the view engine to ejs
+//set the view engine to dust
 app.set('view engine', 'dust');
 //indicate to express where the views directory is
 app.set('views', path.join(__dirname, 'views'));
@@ -20,17 +18,27 @@ app.set('view cache', doCache);
 displayIP();
 console.log("server started on port 3000");
 
-app.get('/', function(req, res) {
-	//render "accueil.dust"(html) using the set render engine (dust)
-	res.render('accueil.dust')
-})
+    app.get('/', function(req, res) {
+	    //render "accueil.dust"(html) using the set render engine (dust)
+	    res.render('accueil.dust')
+    })
+    .get('/exemple', function (req, res) {
+        res.render('exemple.dust', {
+            listTest: [
+                { name: "Bob", age: 21 },
+                { name: "Harry", age: 34 },
+                { name: "John", age: 65 }
+            ],
+            listOfWhat : "type sympatique"
+        });
+    })
     //public files
-.use(express.static(path.join(__dirname, 'public')))
-.use(function(req, res, next){
-	//in case the user asked for an unset page
-	res.writeHead(200, {"Content-Type": "text/html"});
-    res.end('<p>404 not found, bitch</p>');
-});
+    .use(express.static(path.join(__dirname, 'public')))
+    .use(function(req, res, next){
+	    //in case the user asked for an unset page
+	    res.writeHead(200, {"Content-Type": "text/html"});
+        res.end('<p>404 not found, bitch</p>');
+    });
 
 app.listen(3000, "127.0.0.1");
 
