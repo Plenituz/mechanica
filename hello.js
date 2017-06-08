41ebeae4f71b0f7e2a9f3e8aec4b07384d3910bd
waspco_workstation <victor.deleau@gmx.com>
2017-06-08 19:31:55 +0200

    Le CSS fonctionne
    2/3 trois lignes de code en plus dans hello.js pour faire fonctionner le CSS. Les fichiers CSS doivent se trouver dans le dossier public/css/

--- hello.js
+++ hello.js
@@ -5,7 +5,7 @@ var path = require('path');
 var fs = require('fs');
 var hoffman = require('hoffman');
 //listen on port 80 (http)
-var PORT = 80;
+var PORT = 3000;
 
 var app = express();
 //tell express to use EJS render even for html files
@@ -17,12 +17,29 @@ app.set('view engine', 'dust');
 app.set('views', path.join(__dirname, 'views'));
 app.set('view cache', true);
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 console.log("server started on port " + PORT);
 
 app.get('/', function(req, res) {
 	//render "accueil.dust"(html) using the set render engine (dust)
-	res.render('accueil.dust', { names : [{caca : "bob"}, {caca:"billy"}] })
+	res.render('accueil.dust')
 })
+
+//css files
+/*.get('/css/:file', function(req, res){
+	//create a path to /public/imgs/:file
+	var fPath = path.join(__dirname, 'public', 'css',
+		path.basename(req.params.file) );
+	//check if the file exist, if it does serve it otherwise yell
+	if(fs.existsSync(fPath)){
+		res.sendFile(fPath);
+	}else{
+		res.writeHead(200, {"Content-Type": "text/css"});
+		res.end('file ' + req.params.file + " doesn't exist");
+	}
+})*/
+
 //image files
 .get('/imgs/:file', function(req, res){
 	//create a path to /public/imgs/:file
@@ -36,10 +53,11 @@ app.get('/', function(req, res) {
 		res.end('file ' + req.params.file + " doesn't exist");
 	}
 })
+
 .use(function(req, res, next){
-	//in case the user asked for an unset page 
+	//in case the user asked for an unset page
 	res.writeHead(200, {"Content-Type": "text/html"});
     res.end('<p>404 not found, bitch</p>');
 });
 
-app.listen(PORT);
+app.listen(PORT,"127.0.0.1");
